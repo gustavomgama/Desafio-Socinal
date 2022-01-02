19f86cf5e627f3f86cf108b6313e57ec0c9445cb
[Gustavo Gama] <[gutomgama@gmail.com]>
2022-01-02 13:35:21 -0300

    Adiciona autenticação

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    if request.headers['Authorization'].present? && request.headers['Authorization'].split[1] == 'YWRtaW46YWRtaW4='
+      return
+    end
+
+    render json: { message: '401 Unathorized' }, status: :unauthorized
+  end
 end
+
