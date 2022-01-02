d99d8e53b68f42b956252f7e9a5b02994fdc6990
[Gustavo Gama] <[gutomgama@gmail.com]>
2022-01-02 13:31:10 -0300

    Routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'welcome#index'
+  resources :fruits, param: :name, only: %i[index show]
+  resources :service_fruits, param: :name, only: %i[show]
 end
